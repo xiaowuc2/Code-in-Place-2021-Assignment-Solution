da7c9a551dcb4530d2f796c17dc74d33a6e4abd0
Rohit Prasan Mandal <50553530+xiaowuc2@users.noreply.github.com>
2021-05-12 23:36:49 +0530

    Update 2. Even Odd Table.py

--- Diagnostic/2. Even Odd Table.py
+++ Diagnostic/2. Even Odd Table.py
@@ -1,11 +1,11 @@
 MAX_NUMBER = 100
 
 def main():
-    for i in range(1,MAX_NUMBER):
+    for i in range(1,MAX_NUMBER+1):
         if i%2==0:
             print("str(i)"+" is even")
         else: 
-            print("str(i)"+" is even")
+            print("str(i)"+" is odd")
 
 
 if __name__ == "__main__":
