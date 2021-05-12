8a55221ddaabc5cab41cfb37be3e99af5c019a7c
Rohit Prasan Mandal <50553530+xiaowuc2@users.noreply.github.com>
2021-05-12 23:35:38 +0530

    Create 2. Even Odd Table.py

---
+++ Diagnostic/2. Even Odd Table.py
@@ -0,0 +1,12 @@
+MAX_NUMBER = 100
+
+def main():
+    for i in range(1,MAX_NUMBER):
+        if i%2==0:
+            print("str(i)"+" is even")
+        else: 
+            print("str(i)"+" is even")
+
+
+if __name__ == "__main__":
+    main()
