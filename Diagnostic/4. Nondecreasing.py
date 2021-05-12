f2baea1c4b8876c3944faf8111adb020afe66e94
Rohit Prasan Mandal <50553530+xiaowuc2@users.noreply.github.com>
2021-05-13 01:52:54 +0530

    Create 4. Nondecreasing.py

---
+++ Diagnostic/4. Nondecreasing.py
@@ -0,0 +1,21 @@
+def main():
+    a=[]
+    i=0
+    print("Enter a sequence of non-decreasing numbers.")
+    g=float(input("Enter num: "))
+    a. append(g)
+    for i in range(10):
+        c=float(input("Enter num: "))
+        a.append(c)
+        if a[-2]>a[-1]:
+                print("Thanks for playing!")
+                print("Sequence length: "+str(i+1))
+                break
+        else: 
+            i+=1
+    
+
+
+
+if __name__ == "__main__":
+    main()
